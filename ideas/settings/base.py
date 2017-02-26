fdaeab87ff8404ecd7d8a9ff03d1e0682f43f225
chargik <unklerufus@gmail.com>
2017-02-26 14:39:07 +0300

    Email

--- ideas/settings/base.py
+++ ideas/settings/base.py
@@ -27,6 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'unklerufus@gmail.com' #my gmail username
+EMAIL_HOST_PASSWORD = '030a7c4h' #my gmail password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Chargik <unklerufus@gmail.com>"
+
+
+ADMINS = [('chargik', EMAIL_HOST_USER)]
+MANAGERS = ADMINS
 
 # Application definition
 
