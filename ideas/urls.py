6c8f3ef638d28d320cdb3e06af4917b8a221f6e0
chargik <unklerufus@gmail.com>
2017-03-03 22:45:17 +0300

    Update pages for slugfield

--- ideas/urls.py
+++ ideas/urls.py
@@ -21,7 +21,7 @@ from pages.views import HomeView, PageDetailView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^(?P<slug>[\w-]+)$', PageDetailView.as_view(), name='page-detail'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/email/join/$',JoinCreateAPIView.as_view(), name='email-join'),
+    url(r'^(?P<slug>[\w-]+)/$', PageDetailView.as_view(), name='page-detail'),
 ]
